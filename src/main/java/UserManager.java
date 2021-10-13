073a162b98afaf08473c5c9c63ae7fd964c93aaf
TheTonyZhou <hs.zhou@mail.utoronto.ca>
2021-10-12 22:56:57 -0400

    Implemented the class UserManager, CustomerManager, SellerManager for phase 0.

---
+++ src/main/java/UserManager.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+abstract public class UserManager {
+
+    abstract public boolean login(String accName, String password);
+
+    public void addMoney(User user, int money) {
+        user.addMoney(money);
+    }
+
+    public void withdrawMoney(User user, int money) {
+        user.withdray(money);
+    }
+
+    public double checkBalance(User user) {
+        return user.checkBalance;
+    }
+}
