63891be6b5cfd21ebc3fdbc7a77a995b006c259c
TheTonyZhou <hs.zhou@mail.utoronto.ca>
2021-10-16 22:29:41 -0400

    change a comment CustomerManager and add comments in SellerManger.

--- src/main/java/CustomerManager.java
+++ src/main/java/CustomerManager.java
@@ -32,7 +32,7 @@ public class CustomerManager extends UserManager {
      * @param accName  A String that represents the account Name.
      * @param password A String of password that the user typed in.
      * @throws IncorrectCredentialsException Exception if the password doesn't match the account name or there is no
-     *                                       *                                such account name.
+     *                                       such account name.
      */
     @Override
     public void login(String accName, String password) throws IncorrectCredentialsException {
