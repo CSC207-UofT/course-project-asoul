78f7002e2d5aef8fe6d579f52b2af0a4410b9ce7
TheTonyZhou <hs.zhou@mail.utoronto.ca>
2021-10-17 00:55:09 -0400

    add deleteUser method in UserManger class, bug fix in TestCu

--- src/test/java/TestCustomerManager.java
+++ src/test/java/TestCustomerManager.java
@@ -1,63 +1,62 @@
+import org.junit.Test;
+
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class TestCustomerManager {
-    CustomerManager manager;
 
-
-    @org.junit.Before
-    public void setUp() {
-
-        manager = new CustomerManager();
-
-    }
-
-
-
-    @org.junit.Test
+    @Test
     public void loginTest() {
+        CustomerManager manager = new CustomerManager();
         manager.createUser("Customer", "ABC", "123", "nick",
                 "12345");
         Boolean actual = manager.checkUserExist("ABC");
         assert actual.equals(true);
+        manager.deleteUser("ABC");
     }
 
 
     @org.junit.Test
     public void addMoneyTest() {
+        CustomerManager manager = new CustomerManager();
         manager.createUser("Customer", "ABC", "123", "nick",
                 "12345");
         manager.addMoney("ABC", 100);
         double actual = manager.checkBalance("ABC");
         assert actual == 100;
-
+        manager.deleteUser("ABC");
     }
 
 
     @org.junit.Test
     public void withdrawTest() {
+        CustomerManager manager = new CustomerManager();
         manager.createUser("Customer", "ABC", "123", "nick",
                 "12345");
         manager.addMoney("ABC", 100);
         manager.withdrawMoney("ABC", 50);
         double actual = manager.checkBalance("ABC");
         assert actual == 50;
+        manager.deleteUser("ABC");
     }
 
     @org.junit.Test
     public void checkBalanceTest() {
+        CustomerManager manager = new CustomerManager();
         manager.createUser("Customer", "ABC", "123", "nick",
                 "12345");
         manager.addMoney("ABC", 100);
         manager.addMoney("ABC", 100);
         double actual = manager.checkBalance("ABC");
         assert actual == 200;
+        manager.deleteUser("ABC");
     }
 
 
     @org.junit.Test
     public void getUserByAccountNameTest() {
+        CustomerManager manager = new CustomerManager();
         manager.createUser("Customer", "ABC", "123", "nick",
                 "12345");
         HashMap<String, String> actual = manager.getUserByAccountName("ABC");
@@ -69,34 +68,42 @@ public class TestCustomerManager {
         expected.put("phoneNum", "12345");
         expected.put("orderHistory", "");
         assert actual.equals(expected);
+        manager.deleteUser("ABC");
 
     }
 
     @org.junit.Test
     public void getUserTypeTest() {
+        CustomerManager manager = new CustomerManager();
         manager.createUser("Customer", "ABC", "123", "nick",
                 "12345");
         String type = manager.getUserType("ABC");
         assert type.equals("Customer");
+        manager.deleteUser("ABC");
     }
+
 
     @org.junit.Test
     public void checkUserExistTest() {
+        CustomerManager manager = new CustomerManager();
         manager.createUser("Customer", "ABC", "123", "nick",
                 "12345");
         Boolean actual = manager.checkUserExist("ABC");
         assert actual.equals(true);
+        manager.deleteUser("ABC");
 
     }
 
     @org.junit.Test
     public void getCustomersTest() {
+        CustomerManager manager = new CustomerManager();
         manager.createUser("Customer", "ABC", "123", "nick",
                 "12345");
         Customer customer = new Customer("ABC", "123", "nick",
                 "12345");
         int actual = manager.getCustomers().size();
         assert actual == 1;
+        manager.deleteUser("ABC");
 
     }
 
