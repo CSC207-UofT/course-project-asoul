9f5cd995aa2cc96e3712e761d45ffb9f88548d72
TheTomLi <90334025+TheTomLi@users.noreply.github.com>
2021-12-06 15:53:20 -0500

    added activity_foodtruck.xml and activity_market.xml

--- app/src/main/java/com/example/asoul/Activities/FoodTruckActivity.java
+++ app/src/main/java/com/example/asoul/Activities/FoodTruckActivity.java
@@ -1,5 +1,6 @@
 package com.example.asoul.Activities;
 
+import android.text.method.CharacterPickerDialog;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,10 +19,30 @@ public class FoodTruckActivity extends AppCompatActivity {
 
     Button switchToMarket;
     Button switchToUserInfo;
+    Button food0;
+    Button food1;
+    Button food2;
+    Button food3;
+    StringBuilder cartContent;
+    ArrayList<String> menuInfo;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foodtruck);
+
+        ArrayList<Button> btnList = new ArrayList<>();
+        btnList.add(food0);
+        btnList.add(food1);
+        btnList.add(food2);
+        btnList.add(food3);
+        food0 = findViewById(R.id.food0);
+        food1 = findViewById(R.id.food1);
+        food2 = findViewById(R.id.food2);
+        food3 = findViewById(R.id.food3);
+
+
 
 
         switchToMarket = findViewById(R.id.ViewMarket);
@@ -40,6 +61,37 @@ public class FoodTruckActivity extends AppCompatActivity {
             }
         });
 
+        food0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cartContent.append(menuInfo.get(0));
+            }
+        });
+
+        food1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cartContent.append(menuInfo.get(1));
+            }
+        });
+
+        food2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cartContent.append(menuInfo.get(2));
+            }
+        });
+
+        food3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cartContent.append(menuInfo.get(3));
+            }
+        });
+
+        for (int i = 0; i < 4; i++){
+            btnList.get(i).setText(menuInfo.get(i));
+        }
     }
 
     private void setSwitchToMarket(){
--- app/src/main/java/com/example/asoul/Activities/MarketActivity.java
+++ app/src/main/java/com/example/asoul/Activities/MarketActivity.java
@@ -3,15 +3,34 @@ package com.example.asoul.Activities;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import com.example.asoul.R;
+import exceptions.UnknownFoodTruckException;
+import exceptions.UnknownSorterException;
+import sorters.Sorter;
+import sorters.SorterSimpleFactory;
+import use_case.FoodTruckManager;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MarketActivity extends AppCompatActivity {
     Button switchToUserInfo;
     Button switchToUserEdit;
     Button switchToTruckEdit;
     Button switchToExit;
+    Button switchToBlue;
+    Button switchToSushi;
+    Button switchToBrown;
+    Button switchToYellow;
+    private HashMap<String, String> info;
+    private HashMap<Integer, String> pointer;
+    private String sorter;
+    ConstraintLayout marketLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +68,45 @@ public class MarketActivity extends AppCompatActivity {
                 setSwitchToExit();
             }
         });
+
+        switchToSushi = findViewById(R.id.goToSushi);
+        switchToSushi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setSwitchToSushi();
+            }
+        });
+
+
+        switchToBlue = findViewById(R.id.goToBlue);
+        switchToBlue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setSwitchToBlue();
+            }
+        });
+
+
+        switchToBrown = findViewById(R.id.goToBrown);
+        switchToBrown.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setSwitchToBrown();
+            }
+        });
+
+
+        switchToYellow = findViewById(R.id.goToYellow);
+        switchToYellow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setSwitchToYellow();
+            }
+        });
+
+        info = new HashMap<>();
+        pointer = new HashMap<>();
+        sorter = "";
     }
 
 
@@ -72,6 +130,95 @@ public class MarketActivity extends AppCompatActivity {
         startActivity(switchToExitActivityIntent);
     }
 
+    private void setSwitchToBlue(){
+
+        Intent switchToBlueActivityIntent = new Intent(this, LoginActivity.class);
+        startActivity(switchToBlueActivityIntent);
+    }
+
+    private void setSwitchToYellow(){
+        Intent switchToYellowActivityIntent = new Intent(this, LoginActivity.class);
+        startActivity(switchToYellowActivityIntent);
+    }
+
+    private void setSwitchToSushi(){
+        Intent switchToSushiActivityIntent = new Intent(this, LoginActivity.class);
+        startActivity(switchToSushiActivityIntent);
+    }
+
+    private void setSwitchToBrown(){
+        Intent switchToBrownActivityIntent = new Intent(this, LoginActivity.class);
+        startActivity(switchToBrownActivityIntent);
+    }
+
+    private void updateInfo(){
+        info = FoodTruckManager.getActiveFoodTruckDescription();
+        assignPointer();
+    }
+
+    private void assignPointer(){
+        int counter = 1;
+        pointer.clear();
+        try {
+            ArrayList<String> sorted = sortTruckInfo();
+            for (String name : sorted) {
+                pointer.put(counter, name);
+                counter++;
+            }
+        }catch (UnknownSorterException e){ // When Sorter is unknown, assign pointers by the order of the elements in the map
+            for (String name : info.keySet()) {
+                pointer.put(counter, name);
+                counter++;
+            }
+        }
+    }
 
 
+    private ArrayList<String> sortTruckInfo()throws UnknownSorterException{
+        Sorter s = SorterSimpleFactory.constructSorter(sorter);
+        HashMap<String, String> items = new HashMap<>();
+        if(sorter.equals("rating")){
+            for(String key: info.keySet()){
+                items.put(key, Double.toString(FoodTruckManager.getRating(key)));
+            }
+        }else if(sorter.equals("name_length")){
+            for(String key: info.keySet()){
+                items.put(key, FoodTruckManager.getTruckName(key));
+            }
+        }
+        return s.sort(items);
+    }
+
+
+    public void constructOutput(){
+        updateInfo();
+        String upperObjectID = "textview2";
+        for (int i = 1; i <= info.size(); i++) {
+            String key = pointer.get(i);
+            String content = info.get(key);
+            Button newBtn = new Button(MarketActivity.this);
+            marketLayout = (ConstraintLayout) findViewById(R.id.marketLayout);
+            newBtn.setLayoutParams(new ConstraintLayout.LayoutParams(150, 150));
+            newBtn.setId(i);
+            newBtn.setText(content);
+            newBtn.setHeight(170);
+            newBtn.setWidth(400);
+
+            marketLayout.addView(newBtn);
+        }
+    }
+
+    public void viewFoodTruck(String id) throws UnknownFoodTruckException {
+        try{
+            int i = Integer.parseInt(id);
+            String name = pointer.get(i);
+            fc.setFoodtruck(name);
+        }catch (NumberFormatException e){
+            if(FoodTruckManager.existsTruck(id)){
+                fc.setFoodtruck(id);
+            }else{
+                state.append("Unknown id entered! Please enter the name of the foodtruck or its displayed id\n");
+            }
+        }
+    }
 }
