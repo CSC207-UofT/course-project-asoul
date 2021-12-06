134bdbc671774060b411844ea68e1f047e3e271e
Yanke Mao <yanke.mao@mail.utoronto.ca>
2021-12-06 04:02:10 -0500

    Add ChangeMenuActivity

--- app/src/main/java/com/example/asoul/Activities/ChangeMenuActivity.java
+++ app/src/main/java/com/example/asoul/Activities/ChangeMenuActivity.java
@@ -13,5 +13,20 @@ public class ChangeMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_menu);
+
+        Button mbAddFood = findViewById(R.id.cm_addb);
+        //TODO: Add Food to Menu
+
+        Button mbDeleteFood = findViewById(R.id.cm_deleteb);
+        //TODO: Delete Food from Menu
+
+        Button mbBack = findViewById(R.id.cm_back);
+        mbBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChangeMenuActivity.this, ChangeTruckInfoActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
