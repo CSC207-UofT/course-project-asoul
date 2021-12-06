1c2bbfbe8028df45a6d707f57ff100549999c473
WilliamQD <54301360+WilliamQD@users.noreply.github.com>
2021-12-06 05:38:42 -0500

    Added GlobalVariables, edited UserInfoActivity and several activities

--- app/src/main/java/com/example/asoul/Activities/UserInfoActivity.java
+++ app/src/main/java/com/example/asoul/Activities/UserInfoActivity.java
@@ -1,5 +1,6 @@
 package com.example.asoul.Activities;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
@@ -7,15 +8,21 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.asoul.R;
+import exceptions.UnauthorizedAccessException;
+import helper.GlobalVariables;
 import use_case.UserManager;
 
+import java.io.IOException;
+
 public class UserInfoActivity extends AppCompatActivity {
+
+    GlobalVariables globalVariables = (GlobalVariables) this.getApplication();
 
     Button switchToMarketActivity;
     Button switchToChangeUserInfoActivity;
     Button switchToChangeTruckInfoActivity;
     Button switchToOrderHistoryActivity;
-    String Key;
+    String key;
     String username;
     TextView welcomeContent;
     TextView accBalance;
@@ -24,10 +31,40 @@ public class UserInfoActivity extends AppCompatActivity {
 
 
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+
+        key = GlobalVariables.getKey();
+        username = GlobalVariables.getUsername();
+
+        welcomeContent = findViewById(R.id.WelcomeContent);
+        try {
+            welcomeContent.setText("Welcome, " + UserManager.getNickname(username, key));
+        } catch (UnauthorizedAccessException e) {
+            //
+            e.printStackTrace();
+        }
+        accBalance = findViewById(R.id.AccountBalance);
+        try {
+            accBalance.setText("Account Balance: " + UserManager.getBalance(username, key));
+        } catch (UnauthorizedAccessException e) {
+            e.printStackTrace();
+        }
+        phoneNum = findViewById(R.id.PhoneNumber);
+        try {
+            phoneNum.setText("Phone Number: " + UserManager.getPhoneNumber(username, key));
+        } catch (UnauthorizedAccessException e) {
+            e.printStackTrace();
+        }
+        truckName = findViewById(R.id.TruckName);
+        try {
+            accBalance.setText("FoodTruck: " + UserManager.getTruckName(username, key));
+        } catch (UnauthorizedAccessException e) {
+            e.printStackTrace();
+        }
 
 
         switchToMarketActivity = findViewById(R.id.btnViewMarket);
