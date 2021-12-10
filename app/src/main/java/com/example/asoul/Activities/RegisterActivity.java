796276322c9ea210a2e95b810abbfcce391013ca
Yanke Mao <yanke.mao@mail.utoronto.ca>
2021-12-10 17:21:08 -0500

    Edit the interface of register

--- app/src/main/java/com/example/asoul/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/asoul/Activities/RegisterActivity.java
@@ -6,10 +6,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.example.asoul.R;
 import use_case.UserManager;
+
+import java.io.IOException;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -36,14 +39,26 @@ public class RegisterActivity extends AppCompatActivity {
             @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View view) {
-                if (UserManager.createUser(username.getText().toString(),
+                boolean result = UserManager.createUser(username.getText().toString(),
                         password.getText().toString(),
                         nickname.getText().toString(),
-                        phone.getText().toString())){
-                    registerState.setText("Successfully registered new user, you can now proceed to log in!");
+                        phone.getText().toString());
+                try {
+                    UserManager.saveUserDataBase();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                if (result){
+                    String success = "Successfully registered.\n Proceeding to log in...";
+                    Toast.makeText(getApplicationContext(),success,Toast.LENGTH_SHORT).show();
+                    // registerState.setText("Successfully registered new user, you can now proceed to log in!");
+                    setSwitchToLoginActivity();
                 }
                 else{
-                    registerState.setText("User with the given username has already been registered!");
+                    String fail = "Username has already been registered!";
+                    // registerState.setText("User with the given username has already been registered!");
+                    Toast.makeText(getApplicationContext(),fail,Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -57,6 +72,45 @@ public class RegisterActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            UserManager.saveUserDataBase();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        try {
+            UserManager.saveUserDataBase();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            UserManager.saveUserDataBase();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        try {
+            UserManager.saveUserDataBase();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     private void setSwitchToLoginActivity(){
         Intent switchActivityIntent = new Intent(this, LoginActivity.class);
